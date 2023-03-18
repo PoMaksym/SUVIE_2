0bab759662ba3e6c0b3d643e67645f339920a004
maksym.po <maksym.po@room4.team>
2023-03-18 16:56:22 +0200

    changed scroll method

--- pages/homepage.py
+++ pages/homepage.py
@@ -10,12 +10,17 @@ class StartPage(BasePage):
         super().__init__(driver)
         self.constants = HomePage()
 
-    def __scroll_down_page(self, speed=8):
+    def __scroll_down_and_click(self, xpath, speed=5):
         current_scroll_position, new_height = 2, 2
         while current_scroll_position <= new_height:
+            print('current_scroll_position - {}, new_height - {}'.format(current_scroll_position, new_height))
             current_scroll_position += speed
             self.driver.execute_script("window.scrollTo(0, {});".format(current_scroll_position))
-            new_height = self.driver.execute_script("return document.body.scrollHeight")
+            try:
+                self.click(xpath=xpath)
+                return
+            except Exception as e:
+                new_height = self.driver.execute_script("return document.body.scrollHeight")
 
     @wait_until_ok(timeout=5, period=1)
     @log_decorator
@@ -23,15 +28,10 @@ class StartPage(BasePage):
         """Go through checkout"""
         self.click(xpath=self.constants.SHOP_BUTTON_XPATH)
         """Click on Buy now"""
-        self.find_element(xpath=self.constants.SECOND_BUY_NOW_XPATH)
         sleep(3)
-        self.__scroll_down_page()
-        self.click(xpath=self.constants.SECOND_BUY_NOW_XPATH)
+        self.__scroll_down_and_click(xpath=self.constants.SECOND_BUY_NOW_XPATH)
         from pages.second_checkout import SecondCheckoutPage
         return SecondCheckoutPage(self.driver)
-
-    def find_element(self, xpath):
-        pass
 
     @wait_until_ok(timeout=5, period=1)
     @log_decorator
@@ -39,10 +39,8 @@ class StartPage(BasePage):
         """Click on Shop button in homepage"""
         self.click(xpath=self.constants.SHOP_BUTTON_XPATH)
         """Click on Buy now"""
-        self.find_element(xpath=self.constants.THIRD_BUY_NOW_XPATH)
         sleep(3)
-        self.__scroll_down_page()
-        self.click(xpath=self.constants.THIRD_BUY_NOW_XPATH)
+        self.__scroll_down_and_click(xpath=self.constants.THIRD_BUY_NOW_XPATH)
         from pages.third_checkout import ThirdCheckoutPage
         return ThirdCheckoutPage(self.driver)
 
@@ -52,9 +50,7 @@ class StartPage(BasePage):
         """Navigate to first checkout"""
         self.click(xpath=self.constants.SHOP_BUTTON_XPATH)
         """Click on Buy now"""
-        self.find_element(xpath=self.constants.FIRST_BUY_NOW_XPATH)
         sleep(3)
-        self.__scroll_down_page()
-        self.click(xpath=self.constants.FIRST_BUY_NOW_XPATH)
+        self.__scroll_down_and_click(xpath=self.constants.FIRST_BUY_NOW_XPATH)
         from pages.first_checkout import FirstCheckoutPage
         return FirstCheckoutPage(self.driver)
